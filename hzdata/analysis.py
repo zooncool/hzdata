f59f72a4671241cd31c9eb8b0dc83d612a0850a0
LinZhenHua-PC\LinZhenHua <linzhenhua2010@126.com>
2018-03-14 01:17:42 +0800

    修复计算每日成交bug

--- hzdata/analysis.py
+++ hzdata/analysis.py
@@ -14,30 +14,14 @@ class Analysis(HousePipeline):
         pre_day = int(analyze_day - 1)
         today = date.today()
         yesterday = today + timedelta(days=(analyze_day-1))
-        analyze_sql = """
-        SELECT 
-          gmt_created,project_code,building_code,houses,property_name,building_name  
-        FROM
-          building 
-        WHERE building_code IN 
-          (SELECT DISTINCT 
-            bc 
-          FROM
-            (SELECT 
-              building_code AS bc,
-              digest AS MD5,
-              COUNT(digest) cnt 
-            FROM
-              building 
-            WHERE gmt_created >= DATE_FORMAT(DATE_ADD(NOW(),INTERVAL %d DAY),'%%Y-%%m-%%d')
-            AND gmt_created <= DATE_FORMAT(DATE_ADD(NOW(),INTERVAL %d DAY),'%%Y-%%m-%%d')
-            GROUP BY building_code,digest 
-            HAVING COUNT(digest) < 2 
-            ORDER BY bc
-            ) t) AND gmt_created >= DATE_FORMAT(DATE_ADD(NOW(),INTERVAL %d DAY),'%%Y-%%m-%%d')
-            AND gmt_created <= DATE_FORMAT(DATE_ADD(NOW(),INTERVAL %d DAY),'%%Y-%%m-%%d')
-        ORDER BY building_code 
-        """ % (pre_day, analyze_day, pre_day, analyze_day)
+        current_sql = """
+        SELECT project_code,building_code,property_name,building_name,houses,digest,gmt_created FROM building 
+        where gmt_created = DATE_FORMAT(DATE_ADD(NOW(),INTERVAL %d DAY),'%%Y-%%m-%%d') 
+        """ % analyze_day
+        previous_sql = """
+        SELECT project_code,building_code,property_name,building_name,houses,digest,gmt_created FROM building 
+        where gmt_created = DATE_FORMAT(DATE_ADD(NOW(),INTERVAL %d DAY),'%%Y-%%m-%%d') 
+        """ % pre_day
         save_sql = """
         insert into contract(gmt_created,project_code,building_code,property_name,building_name,house_code,new_state, 
                     old_state, contract_date) 
@@ -47,32 +31,33 @@ class Analysis(HousePipeline):
         '%Y-%m-%d')
         """
         try:
-            self.cursor.execute(analyze_sql)
-            buildings_records = self.cursor.fetchall()
-            building_code_temp = 0
-            record_temp = []
-            for record in buildings_records:
-                project_code = record[1]
-                building_code = record[2]
-                houses = record[3]
-                property_name = record[4]
-                building_name = record[5]
-                if building_code != building_code_temp:
-                    building_code_temp = building_code
-                    record_temp = record
-                else:
-                    houses_temp = record_temp[3]
-                    if houses == "" or houses_temp == "":
-                        continue
-                    house_list_temp = str(houses_temp).split(",")
-                    house_list = str(houses).split(",")
-                    for (house_temp, house) in zip(house_list_temp, house_list):
-                        if house_temp != house:
-                            house_code = str(house).split("|")[0]
-                            new_state = str(house).split("|")[1]
-                            old_state = str(house_temp).split("|")[1]
-                            self.cursor.execute(save_sql, (datetime.now(), project_code, building_code, property_name,
-                                                           building_name, house_code, new_state, old_state, yesterday))
+            self.cursor.execute(current_sql)
+            current_buildings = self.cursor.fetchall()
+            self.cursor.execute(previous_sql)
+            previous_buildings = self.cursor.fetchall()
+            for current_building in current_buildings:
+                cur_project_code = current_building[0]
+                cur_building_code = current_building[1]
+                cur_property_name = current_building[2]
+                cur_building_name = current_building[3]
+                cur_houses = current_building[4]
+                cur_digest = current_building[5]
+                for previous_building in previous_buildings:
+                    pre_building_code = previous_building[1]
+                    pre_houses = previous_building[4]
+                    pre_digest = previous_building[5]
+                    if cur_building_code == pre_building_code and cur_digest != pre_digest:
+                        cur_house_array = str(cur_houses).split(",")
+                        pre_house_array = str(pre_houses).split(",")
+                        for index, cur_house in enumerate(cur_house_array):
+                            if cur_house != pre_house_array[index]:
+                                house_code = str(cur_house).split("|")[0]
+                                new_state = str(cur_house).split("|")[1]
+                                old_state = str(pre_house_array[index]).split("|")[1]
+                                self.cursor.execute(save_sql,
+                                                (datetime.now(), cur_project_code, cur_building_code,
+                                                 cur_property_name, cur_building_name, house_code, new_state,
+                                                 old_state, yesterday))
             self.cursor.execute(delete_houses)
             self.connect.commit()
         except Exception as error:
