3281c038df7ecff3d332c677371f1495a62c681d
LinZhenHua-PC\LinZhenHua <linzhenhua2010@126.com>
2018-11-28 02:34:32 +0800

    代理过滤splsh链接

--- hzdata/middlewares.py
+++ hzdata/middlewares.py
@@ -75,11 +75,13 @@ class RandomUserAgent(object):
 class ProxyMiddleware(object):
     def process_request(self, request, spider):
         proxy = random.choice(settings.PROXIES)
-        if proxy['user_pass'] is not None:
+        if ':8050' not in str(request.url):
             request.meta['proxy'] = "http://%s" % proxy['ip_port']
-            encoded_user_pass = base64.encodestring(proxy['user_pass'])
-            request.headers['Proxy-Authorization'] = 'Basic ' + encoded_user_pass
-            print("**************ProxyMiddleware have pass************" + proxy['ip_port'])
-        else:
-            print("**************ProxyMiddleware no pass************" + proxy['ip_port'])
-            request.meta['proxy'] = "http://%s" % proxy['ip_port']
+        # if proxy['user_pass'] is not None:
+        #     request.meta['proxy'] = "http://%s" % proxy['ip_port']
+        #     encoded_user_pass = base64.encodestring(proxy['user_pass'])
+        #     request.headers['Proxy-Authorization'] = 'Basic ' + encoded_user_pass
+        #     # print("**************ProxyMiddleware have pass************" + proxy['ip_port'])
+        # else:
+        #     # print("**************ProxyMiddleware no pass************" + proxy['ip_port'])
+        #     request.meta['proxy'] = "http://%s" % proxy['ip_port']
